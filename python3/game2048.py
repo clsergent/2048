adeb63bde6bece5ec3958eb937d68b70cedb8423
clsergent <clsergent@free.fr>
2021-02-02 21:32:07 +0400

    Minor update Removed dependancy of Square to Engine to ensure a better separation between the GUI and the core of the program

--- python3/game2048.py
+++ python3/game2048.py
@@ -25,14 +25,15 @@ ABOUT_TITLE = 'A propos'
 ABOUT_MESSAGE = f'{__app_name__}\nVersion: {__version__}\nAuthor: {__author__}\nLicense: {__license__}'
 
 
+class Move(enum.Enum):
+    LEFT = 0
+    RIGHT = 1
+    UP = 2
+    DOWN = 3
+
+
 class Engine(object):
     """ Game core """
-    class Move(enum.Enum):
-        LEFT = 0
-        RIGHT = 1
-        UP = 2
-        DOWN = 3
-
     def __init__(self, size=4):
         self.gridSize = size
         self.start()
@@ -42,13 +43,13 @@ class Engine(object):
         return self._score
 
     def _compose(self, x: int, y: int, side: Move):
-        if side is None or side is Engine.Move.LEFT:
+        if side is None or side is Move.LEFT:
             return x, y
-        if side is Engine.Move.RIGHT:
+        if side is Move.RIGHT:
             return self.gridSize - 1 - x, y
-        elif side is Engine.Move.UP:
+        elif side is Move.UP:
             return self.gridSize - 1 - y, x
-        elif side is Engine.Move.DOWN:
+        elif side is Move.DOWN:
             return y, self.gridSize - 1 - x
 
     def getGridItem(self, x, y, side=None) -> int:
@@ -90,7 +91,7 @@ class Engine(object):
 
     def testEnd(self):
         # test whether the game is over or not (which happens if no 0 remains)
-        for move in self.Move:
+        for move in Move:
             if self.moveGridItems(move, apply=False) or self.sumGridItems(move, apply=False):
                 return False
         return True
@@ -146,83 +147,82 @@ class Square(QtWidgets.QGraphicsItem):
     """ A graphic square """
     def __init__(self, parent: QtWidgets.QGraphicsView, x: int, y: int):
         QtWidgets.QGraphicsItem.__init__(self)
-        self._parent= parent
+        self.parent= parent
         self.x = x
         self.y = y
-    
-    def getValue(self) -> str:
-        """ return a string representation of the square value """
-        value= self._parent.engine.getGridItem(self.x, self.y)
-        if value > 0:
-            return str(value)
-        return ''
-    
-    def getColor(self):
+        self._value = 0
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+
+    @property
+    def color(self):
         """ return a RGB color based on the square value"""
-        value= self._parent.engine.getGridItem(self.x, self.y)
         # return int(abs(cos(value) * 256**3) + abs(sin(value+seed) * 256**2) + abs(tan(value+seed) * 256))
-        return int(abs(cos(value + COLOR_SEED) * 256 ** 3))
+        return int(abs(cos(self.value + COLOR_SEED) * 256 ** 3))
         
     def boundingRect(self):
-        size= self._parent.squareSize()
+        size= self.parent.squareSize()
         return QtCore.QRectF(self.x*size[0], self.y*size[1], size[0], size[1])
 
     def innerRect(self):
-        size = self._parent.squareSize()
+        size = self.parent.squareSize()
         return QtCore.QRectF(self.x * size[0]+2, self.y * size[1]+2, size[0]-2, size[1]-2)
 
     def paint(self, painter, option, widget):
-        size = self._parent.squareSize()
-
-
+        size = self.parent.squareSize()
         # paint the square
         painter.setOpacity(0.5)
-        painter.setBrush(QtGui.QColor(self.getColor()))
+        painter.setBrush(QtGui.QColor(self.color))
         painter.setCompositionMode(QtGui.QPainter.CompositionMode_SourceOver)
         painter.drawRoundedRect(self.innerRect(), 10, 10)
 
         # paint text
         painter.setOpacity(1)
-        painter.setFont(QtGui.QFont("Arial", (size[0] + size[1])//4))
+        painter.setFont(QtGui.QFont("Arial", (size[0] + size[1])//6))
         painter.drawText(self.x * size[0] + 1, self.y * size[1] + 1,
                          size[0] - 1, size[1] - 1,
-                         QtCore.Qt.AlignCenter, self.getValue())
+                         QtCore.Qt.AlignCenter, str(self.value))
 
 
 class Frame(QtWidgets.QGraphicsView):
     """ Game main window """
-    def __init__(self, scene=None, grid_size=4):
-        QtWidgets.QGraphicsView.__init__(self)
-        self.setWindowFlags(QtCore.Qt.Window)
-        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-        self.setAttribute(QtCore.Qt.WA_NoSystemBackground)
-
+    def __init__(self, parent=None, scene=None, grid_size=4):
+        QtWidgets.QGraphicsView.__init__(self, parent)
+        # attributes
         self.application = QtWidgets.QApplication.instance()
-
-        if type(scene) != QtWidgets.QGraphicsScene:
-            self.scene= QtWidgets.QGraphicsScene()
-        else:
-            self.scene = scene
-
+        self.scene= QtWidgets.QGraphicsScene()
+        self.setScene(self.scene)
         self.engine = Engine(grid_size)
-
-        # Window settings
-        self.setRenderHint(QtGui.QPainter.Antialiasing)
-        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.setAlignment(QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
-        self.setMinimumSize(100*self.engine.gridSize,75*self.engine.gridSize)
-        self.resize(100*self.engine.gridSize,75*self.engine.gridSize)
-        self.squares = [[0] * self.engine.gridSize for i in range(self.engine.gridSize)]
+        self.squares = [[Square] * self.engine.gridSize for i in range(self.engine.gridSize)]
 
         for x in range(self.engine.gridSize):
             for y in range(self.engine.gridSize):
                 self.squares[x][y]= Square(self, x, y)
                 self.scene.addItem(self.squares[x][y])
 
-        self.setScene(self.scene)
+        # window settings
+        self.setWindowFlags(QtCore.Qt.Window)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.setAttribute(QtCore.Qt.WA_NoSystemBackground)
+        #self.setStyleSheet('background-color: transparent;')
+        self.setBackgroundBrush(QtGui.QColor(QtCore.Qt.transparent))
+        self.setRenderHint(QtGui.QPainter.Antialiasing)
         self.setWindowTitle("2048")
-        self.update()
+
+        # layout and scrollbars
+        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.setAlignment(QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+
+        # size
+        self.setMinimumSize(100*self.engine.gridSize,75*self.engine.gridSize)
+        self.resize(100*self.engine.gridSize,75*self.engine.gridSize)
 
         # Action settings
         self._menuBar = QtWidgets.QMenuBar(self)
@@ -233,29 +233,36 @@ class Frame(QtWidgets.QGraphicsView):
         
         self._up= QtWidgets.QAction(self)
         self._up.setShortcut('Up')
-        self._up.triggered.connect(lambda: self.move(Engine.Move.UP))
+        self._up.triggered.connect(lambda: self.move(Move.UP))
         self.addAction(self._up)
         
         self._down= QtWidgets.QAction(self)
         self._down.setShortcut('Down')
-        self._down.triggered.connect(lambda: self.move(Engine.Move.DOWN))
+        self._down.triggered.connect(lambda: self.move(Move.DOWN))
         self.addAction(self._down)
         
         self._left= QtWidgets.QAction(self)
         self._left.setShortcut('Left')
 
-        self._left.triggered.connect(lambda: self.move(Engine.Move.LEFT))
+        self._left.triggered.connect(lambda: self.move(Move.LEFT))
         self.addAction(self._left)
         
         self._right= QtWidgets.QAction(self)
         self._right.setShortcut('Right')
-        self._right.triggered.connect(lambda: self.move(Engine.Move.RIGHT))
+        self._right.triggered.connect(lambda: self.move(Move.RIGHT))
         self.addAction(self._right)
 
+        self.update()
         self.show()
 
     def update(self):
+        for x in range(self.engine.gridSize):
+            for y in range(self.engine.gridSize):
+                self.squares[x][y].value = self.engine.getGridItem(x, y)
+        self.scene.update()
         self.updateIcon()
+        self.application.sendPostedEvents()
+        self.application.processEvents()
         self.scene.update()
 
     def updateTitle(self):
@@ -272,7 +279,7 @@ class Frame(QtWidgets.QGraphicsView):
         """ return the graphic size of a square """
         return self.width() // self.engine.gridSize, self.height() // self.engine.gridSize
     
-    def move(self, side: Engine.Move):
+    def move(self, side: Move):
         """ move the elements according to the given side """
         if self.engine.moveGridItems(side) | self.engine.sumGridItems(side):
             self.engine.newGridItem()
